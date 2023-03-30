bab5181b0d5b1950ab6c1c5369717f00a56587e2
Pranjanshinde <pranjanshinde10@gmail.com>
2023-03-30 19:34:20 +0530

    productpage

--- flower/src/App.js
+++ flower/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Productdetail from './pages/Productdetails';
 
 function App() {
   return (
     <div className="App">
-      
+      <Productdetail/>
     </div>
   );
 }
