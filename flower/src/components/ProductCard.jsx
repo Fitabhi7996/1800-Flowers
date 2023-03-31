ebd6c7559f99ec61a5d5eb6126b29472559b1d45
unknown <akshaydeshmukh9996@gmail.com>
2023-03-31 13:50:40 +0530

    productpage

--- flower/src/components/ProductCard.jsx
+++ flower/src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { color } from 'framer-motion'
 import React from 'react'
 
 const ProductCard = ({image,title,price,category,id}) => {
@@ -6,10 +7,12 @@ const ProductCard = ({image,title,price,category,id}) => {
 
 <div >
       <img src={image} alt=""  style={{width:"100%",height:"100%"}}/>
-      <img  style={{width:"50%",textAlign:"center"}} src="https://images.contentstack.io/v3/assets/bltdd99f24e8a94d536/blt8d4549d3cac15860/61e09d4f2e109d6c649d4aa4/PP_EligibleIcon.svg" alt="" />
-      <h1>{title}</h1>
-      <h3 style={{textShadow:"initial"}}>{price}</h3>
-      <h3>{category}</h3>
+      <img  style={{width:"70%",paddingLeft:"150px"}} src="https://images.contentstack.io/v3/assets/bltdd99f24e8a94d536/blt8d4549d3cac15860/61e09d4f2e109d6c649d4aa4/PP_EligibleIcon.svg" alt="" />
+      <h1 style={{fontWeight:"bold"}}>{title}</h1>
+      <h3 style={{fontWeight:"bold"}}>${price}</h3>
+      
+      
+      
 
       
     </div>
--- flower/src/pages/Products.jsx
+++ flower/src/pages/Products.jsx
@@ -1,12 +1,35 @@
 import React, { useState } from "react";
+import {
+  
+  
+ 
+ 
+  Button,
+  
+  
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  WrapItem,
+} from "@chakra-ui/react";
+
+
+import {
+  
+  ChevronDownIcon,
+
+} from "@chakra-ui/icons";
 
 import ProductCard from "../components/ProductCard";
 
 export const Products = () => {
+  const [cat,setCat]=React.useState("Easter")
   const [data, setData] = useState([]);
 
   const getData = () => {
-    fetch("https://wild-blue-lemur-sari.cyclic.app/flowers")
+    fetch(`https://wild-blue-lemur-sari.cyclic.app/flowers?category=${cat}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -16,27 +39,315 @@ export const Products = () => {
 
   React.useEffect(() => {
     getData();
-  }, []);
+  }, [cat]);
+
+  
   return (
-    <div>
+    <>
+    <div style={{width:"auto",display:"flex",}}>
       {/* <Navbar/> */}
+
+      <div style={{ display: "flex", gap: "25px", width:"30%" }}>
+        <div
+          style={{
+            // border: "1px solid teal",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "10px",
+              //   border: "1px solid red",
+              height: "290px",
+              width: "90%",
+              backgroundColor: "#734f96",
+              marginTop: "10px",
+              borderRadius: "8px",
+              paddingLeft:"20px",
+              textAlign:"center",
+              alignContent:"center"
+
+             
+            }}
+          >
+            <h3 style={{ color: "white", paddingTop: "15px", }}>
+              <Text as="b">FIND AVAILABLE PRODUCTS!</Text>
+            </h3>
+            <p
+              style={{
+                marginLeft: "15px",
+                fontSize: "11px",
+                color: "white",
+                padding: "15px",
+                textAlign: "center",
+                
+              }}
+            >
+              ENTER DELIVERY ZIP CODE OR ADDRESS*
+            </p>
+            <input
+              style={{
+                height: "45px",
+                width: "215px",
+                borderRadius: "4px",
+                // margin: "-15px",
+                textAlign:"center",
+
+                marginTop: "-10px",
+              }}
+              type="text"
+              placeholder="Enter a location"
+            />
+            <p
+              style={{
+                marginLeft: "15px",
+                fontSize: "11px",
+                color: "white",
+                padding: "15px",
+                textAlign: "center",
+              }}
+            >
+              LOCATION TYPE:
+            </p>
+            <Menu>
+              <MenuButton
+                style={{
+                  height: "45px",
+                  width: "220px",
+                  textAlign: "center",
+                  marginTop: "-10px",
+                }}
+                as={Button}
+                rightIcon={<ChevronDownIcon />}
+              >
+                Residence
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Residence</MenuItem>
+                <MenuItem>Business</MenuItem>
+                <MenuItem>Funeral home</MenuItem>
+                <MenuItem>Hospital</MenuItem>
+                <MenuItem>Apartment</MenuItem>
+                <MenuItem>School</MenuItem>
+                <MenuItem>Church</MenuItem>
+              </MenuList>
+            </Menu>
+            <WrapItem>
+              <Button
+                style={{
+                  height: "45px",
+                  width: "221px",
+                  marginLeft: "120px",
+                  marginTop: "10px",
+                  textAlign:"center"
+                }}
+                colorScheme="whatsapp"
+              >
+                GO
+              </Button>
+            </WrapItem>
+          </div>
+
+          
+          <div
+            style={{
+              textAlign: "left",
+              paddingTop: "20px",
+              border: "3px solid #636363",
+              fontSize:"25px",
+              height: "360px",
+              width: "90%",
+              marginTop: "10px",
+              cursor: "pointer",
+            }}
+          >
+            <div style={{ marginLeft: "15px" }}>
+              <Text as="b">SHOP ROSES BY COLOR</Text>
+
+              <u>
+                <p style={cat=="Easter"?{color:"blue"}:null} onClick={()=>setCat("Easter")}>Easter Color Rose</p>
+              </u>
+              
+              <u>
+                <p style={cat=="Red Rose"?{color:"red"}:null }onClick={()=>setCat("Red Rose")}>Red Color Rose</p>
+              </u>
+              <u>
+                <p style={cat=="pink Rose"?{color:"green"}:null }onClick={()=>setCat("Pink Rose")}>Pink Color Rose</p>
+              </u>
+              <u>
+                <p style={cat=="Orange Rose"?{color:"orange"}:null }onClick={()=>setCat("Orange Rose")} >Orange Color Rose</p>
+              </u>
+              <u>
+                <p style={cat=="White Rose"?{color:"white"}:null }onClick={()=>setCat("White Rose")}>White Color Rose</p>
+              </u>
+              <u>
+                <p style={cat=="Purple Rose"?{color:"purple"}:null }onClick={()=>setCat("Purple Rose")}>Purple Color Rose</p>
+              </u>
+             
+              <u>
+                <p style={cat=="Yellow Rose"?{color:"yellow"}:null }onClick={()=>setCat("Yellow Rose")}>Yellow Color Rose</p>
+              </u>
+              
+            </div>
+          </div>
+          <div
+            style={{
+              border: "3px solid  #636363",
+              marginLeft: "12px",
+              height: "550px",
+              width: "90%",
+              marginTop: "20px",
+              fontSize:"25px"
+            }}
+          >
+            <div
+              style={{
+                cursor: "pointer",
+                marginLeft: "15px",
+                textAlign: "left",
+                marginTop: "15px",
+              }}
+            >
+              <Text as="b">SHOP EASTER COLLECTIONS</Text>
+              <u>
+                <p>Flower Delivery Same-Day</p>
+              </u>
+              
+              <u>
+                <p>Spring Flower Delivery</p>
+              </u>
+              <u>
+                <p>Bulb Delivery </p>
+              </u>
+              <u>
+                <p>Easter Gift Basket Delivery</p>
+              </u>
+              <u>
+                <p>Easter Centerpieces & Decorations</p>
+              </u>
+              <u>
+                <p>Easter Lilies & Easter Plants</p>
+              </u>
+              <u>
+                <p>Easter Baskets for Kids</p>
+              </u>
+              <u>
+                <p>Easter Fruit Arrangements</p>
+              </u>
+              <u>
+                <p>Unique Easter Gifts & Keepsakes</p>
+              </u>
+               <u>
+                <p> Easter Gifts & Keepsakes</p>
+              </u>
+               <u>
+                <p>Easter Gifts & birthday</p>
+              </u>
+              <u>
+                <p>Easter fruit Arrangemet</p>
+              </u>
+            </div>
+          </div>
+          <div
+            style={{
+              border: "3px solid  #636363",
+              marginLeft: "12px",
+              height: "550px",
+              width: "90%",
+              marginTop: "20px",
+              fontSize:"25px"
+            }}
+          >
+            <div
+              style={{
+                cursor: "pointer",
+                marginLeft: "15px",
+                textAlign: "left",
+                marginTop: "15px",
+              }}
+            >
+              <Text as="b">POPULAR EASTER FLOWERS 2023</Text>
+              <u>
+                <p>Easter Lilies</p>
+              </u>
+              
+              <u>
+                <p>Tulips</p>
+              </u>
+              <u>
+                <p>Bulbs </p>
+              </u>
+              <u>
+                <p>Roses</p>
+              </u>
+              <u>
+                <p>Hydrangeas</p>
+              </u>
+              <u>
+                <p>Sunflowers</p>
+              </u>
+              <u>
+                <p>Daisies</p>
+              </u>
+              <u>
+                <p>All Lilies</p>
+              </u>
+              <u>
+                <p>Gardenias</p>
+              </u>
+               <u>
+                <p> Carnations</p>
+              </u>
+               <u>
+                <p>Gerbera Daisies</p>
+              </u>
+              <u>
+                <p>Easter fruit Arrangemet</p>
+              </u>
+            </div>
+            </div>
+         
+         
+        </div>
+
+        {/* <div> */}
+        {/* <div
+            style={{
+             border: "1px solid red",
+              height: "8000px",
+              width: "1030px",
+              backgroundColor: "#FFFFFF",
+            }}
+          >
+            
+          </div> */}
+
+        {/* </div> */}
+        
+      </div>
+
+      
 
       
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(4,1fr)",
+          gridTemplateColumns: "repeat(3,1fr)",
           gap: "50px",
           columnGap:"20px",
           textAlign: "center",
-          rowGap:"80px"
+          rowGap:"80px",
+          width:"85%",
+          height:"10%"
         }}
       >
         {data.map((item) => (
           <ProductCard key={item.id} {...item} />
-        ))}{" "}
+        ))}
       </div>
       
     </div>
+    </>
   );
+
 };
