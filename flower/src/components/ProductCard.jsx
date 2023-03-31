1b3b274f19cfb44ba9651c60ac1c4f38c7d28ea2
unknown <akshaydeshmukh9996@gmail.com>
2023-03-31 13:58:17 +0530

    akshay

--- flower/src/components/ProductCard.jsx
+++ flower/src/components/ProductCard.jsx
@@ -8,8 +8,8 @@ const ProductCard = ({image,title,price,category,id}) => {
 <div >
       <img src={image} alt=""  style={{width:"100%",height:"100%"}}/>
       <img  style={{width:"70%",paddingLeft:"150px"}} src="https://images.contentstack.io/v3/assets/bltdd99f24e8a94d536/blt8d4549d3cac15860/61e09d4f2e109d6c649d4aa4/PP_EligibleIcon.svg" alt="" />
-      <h1 style={{fontWeight:"bold"}}>{title}</h1>
-      <h3 style={{fontWeight:"bold"}}>${price}</h3>
+      <h1 style={{fontWeight:"bold",fontSize:"20px"}}>{title}</h1>
+      <h3 style={{fontWeight:"bold", fontSize:"20px"}}>${price}</h3>
       
       
       
