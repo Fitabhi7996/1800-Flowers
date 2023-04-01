c1aebb4ff535bd3dcb62858548766d1f186d02bb
Abhishek Ghodmare <110329801+Fitabhi7996@users.noreply.github.com>
2023-04-01 23:33:59 +0530

    Update App.js

--- flower/src/App.js
+++ flower/src/App.js
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 import './App.css';
+import Footer from './Components/Landing Page/Footer';
+import Navbar from './Components/Landing Page/Navbar';
+import BirthdayCake from './pages/product';
 import { Products } from './pages/Products';
 
 
