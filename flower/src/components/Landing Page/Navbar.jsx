27ba67480ec7e6d30c8af51c4bc51a0321c10a40
RahulRathod5181 <rahulrathod5181@gmail.com>
2023-04-01 22:50:14 +0530

    Day-5 work Rahul

--- flower/src/components/Landing Page/Navbar.jsx
+++ flower/src/components/Landing Page/Navbar.jsx
@@ -4,21 +4,21 @@ import logo from "./images/logo.png"
 import signIn from "./images/sign-in.svg"
 import truck from "./images/truck.svg"
 import cartIcon from "./images/cartIcon.svg"
+import Dropdown from "./Dropdown"
 
-import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons"
-import DropDown from './Dropdown'
+
 const Navbar = () => {
   return (
     <div style={{ marginBottom: "15px" }}>
       <div className={style.first} >
         <div>
-
           <img src="https://images.contentstack.io/v3/assets/blt95e0138311135214/blt13b0f02cd71877df/5f08974a8b0e59084c9f7570/hd-logo-v3.svg?height=32&width=78" alt="harry" />
         </div>
-        <div>
 
+        <div>
           <img src="https://images.contentstack.io/v3/assets/blt95e0138311135214/bltac334446d6f9e89c/5f96fc4f2425cd7a8af6845e/pmall_svg-logo-v2.svg?height=32&width=78" alt="Mall" />
         </div>
+
         <div>
 
           <img src="https://images.contentstack.io/v3/assets/blt95e0138311135214/bltf0b1021465476e4f/5f08974b0d1d2008538ce15a/SharisLogo_r.svg?height=32&width=78" alt="Berries" />
@@ -51,6 +51,7 @@ const Navbar = () => {
 
           <img src="https://images.contentstack.io/v3/assets/blt95e0138311135214/blta2ba2ac7606f6c15/5f08974bceebbf7e87c30215/sy-logo-v2.svg?height=32&width=78" alt="stock" />
         </div>
+
         <div>
 
           <img src="https://images.contentstack.io/v3/assets/blt0281495b20b504a0/blta4f709f645453ff0/61b4eeeea000c90ecc6e15bd/VC_Logo_w_tag.svg?height=32&width=78" alt="vitalchoice" />
@@ -83,7 +84,7 @@ const Navbar = () => {
 
       {/* ====================================DropDown=============================================== */}
       <div className={style.dropdown}>
-        <DropDown/>
+        <Dropdown/>
       </div>
     </div>
 
